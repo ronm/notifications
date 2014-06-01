4879e2591ae9cae23cc2a549f1185420efc7cc97
ronm <ronmarcelle@gmail.com>
2014-05-31 20:05:08 -0700

    Add Status Report messaging

--- js/notifications_admin.js
+++ js/notifications_admin.js
@@ -8,8 +8,12 @@
 		return false;
 	}
 	
-	var NotificationAuthorize = function() {
-		Notification.requestPermission(function (permission) {
+	var $notificationsForm = $( "#notifications-admin"),
+		$notificationsTouched = $( "#notifications-grant-access-touched" ),
+		$permissionButton = $("#edit-notifications-grant-access"),
+		NotificationAuthorize = function() {
+			Notification.requestPermission(function (permission) {
+			
 			if(!('permission' in Notification)) { Notification.permission = permission; }
 			if (permission === "granted") { 
 				successPerms();
@@ -18,11 +22,15 @@
 				failedPerms();
 				return false;
 			}
-		})
+		});
 	},
-	$permissionButton = $("#edit-notifications-grant-access"),
-	successPerms = function () { $permissionButton.addClass('success').html('Notifications On').prop('disabled', true); },
+	successPerms = function () { 
+		$permissionButton.addClass('success').html('Notifications On').prop('disabled', true);
+		$notificationsTouched.val( 1 );
+	},
 	failedPerms = function () { $permissionButton.addClass('warning').html('Notifications Off'); };
+	
+	$notificationsTouched.val( 0 );
 	
 	if ( Notification.permission === "granted" ) { successPerms(); }
 	else if ( Notification.permission === "denied" ) { failedPerms(); }
